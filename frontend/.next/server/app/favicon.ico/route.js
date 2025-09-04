1b248d93e5b44ee3d00b578c08352ae5a1c0f5b0
Kushagra3062 <kushagrasingh3062@gmail.com>
2025-09-05 00:16:42 +0530

    Merged Local Repo With Remote Repo

---
+++ frontend/.next/server/app/favicon.ico/route.js
@@ -0,0 +1,7 @@
+const CHUNK_PUBLIC_PATH = "server/app/favicon.ico/route.js";
+const runtime = require("../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_b9868160._.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__6e2d2183._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
