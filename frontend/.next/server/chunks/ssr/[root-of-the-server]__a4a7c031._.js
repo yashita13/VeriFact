b6d409d334d505e471978fa1844dc9fb29fd8779
Tanmay24-ya <dixitt552@gmail.com>
2025-09-21 23:08:16 +0530

    changing

--- frontend/.next/server/chunks/ssr/[root-of-the-server]__a4a7c031._.js
+++ frontend/.next/server/chunks/ssr/[root-of-the-server]__a4a7c031._.js
@@ -753,7 +753,7 @@ function ResultsPage() {
                                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                             className: "bg-gradient-to-r from-pink-600 to-purple-600 rounded-2xl shadow-xl text-center p-6 hover:scale-105 transition",
                                             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {
-                                                href: "/",
+                                                href: "/analyze",
                                                 className: "font-bold text-lg text-white",
                                                 children: "🔄 New Check"
                                             }, void 0, false, {
--- frontend/.next/static/chunks/_5a1de79f._.js
+++ frontend/.next/static/chunks/_5a1de79f._.js
@@ -749,7 +749,7 @@ function ResultsPage() {
                                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                             className: "bg-gradient-to-r from-pink-600 to-purple-600 rounded-2xl shadow-xl text-center p-6 hover:scale-105 transition",
                                             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                                                href: "/",
+                                                href: "/analyze",
                                                 className: "font-bold text-lg text-white",
                                                 children: "🔄 New Check"
                                             }, void 0, false, {
